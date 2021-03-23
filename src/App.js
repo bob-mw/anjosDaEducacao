1c2d3af60f529bd88e441f0d1ed4ba11e88db1b1
Bob Wendell <wend3llsouza@gmail.com>
2021-03-22 22:15:52 -0300

    :package: example angel.svg

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Provider from './context/Provider';
 import Card from './components/Card';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
+import angel from './img/angel.svg';
 
 function App(){
     return (
       <Provider>
         <NavBar />
         <main>
+          <img src={ angel } />
           <Container>
           <Card />
           </Container>
