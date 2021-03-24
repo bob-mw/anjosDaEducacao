0b2c1df7d7b74d8ccfd3bbddd571a51d73705ae2
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-24 07:47:29 -0300

    Material Schema

---
+++ back/src/entities/material.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Schema } from 'mongoose'
+
+type Material = Document & {};
+
+const MaterialSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    cost: {
+      type: Number,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+  
+export default mongoose.model<Material>('Material', MaterialSchema)
+  
