e9928db21ee6cf83a53fa1b7aafe59e1d8f2f5e7
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-24 10:19:09 -0300

    MongoDB connection

--- back/src/database/index.ts
+++ back/src/database/index.ts
@@ -1,3 +1,8 @@
 import { createConnection } from 'typeorm'
+import { connect } from 'mongodb'
 
 createConnection()
+connect('localhost:27017/angels', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
