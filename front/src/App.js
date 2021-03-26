fb4cce1fa2d628768c2446500233378e8b004535
marcus-v-rodrigues <marcus.v.rodrigues@usp.br>
2021-03-26 14:37:57 -0300

    Created Main Page Component

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import Provider from './context/Provider';
-import Card from './components/Card';
-import NavBar from './components/NavBar';
-import { Container } from 'react-bootstrap';
-import angel from './img/angel.svg';
+import Main from './components/pages/Main'
 
 function App(){
     return (
-      <Provider>
-        <NavBar />
-        <main className="gradient-sky">
-          <Container className="mr-5">
-          <img src={ angel } />
-          <Card />
-          </Container>
-        </main>
-      </Provider>
+      <Main/>
     );
 }
 
