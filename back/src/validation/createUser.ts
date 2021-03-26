71e7b774cca7bee38a1c90ef66ff13569e3eca6e
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-26 01:13:51 -0300

    Update CreateUser validation

--- back/src/validation/createUser.ts
+++ back/src/validation/createUser.ts
@@ -4,6 +4,10 @@ const schema = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
   password: yup.string().required(),
+  confirmPassword: yup.string().test('passwords-match', function (value) {
+    const { password } = this.parent
+    return password === value
+  }),
   cpf: yup.string().required().length(11),
   phone: yup.string().required()
 })
