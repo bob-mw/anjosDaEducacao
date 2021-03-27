500a09ead422c0c027af6c0de0ccc23846d813ce
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-27 14:23:12 -0300

    Sidebar structure created

--- front/src/components/Menu/index.jsx
+++ front/src/components/Menu/index.jsx
@@ -4,11 +4,14 @@ import * as S from './styled'
 const Menu = () => {
   
     return (
-        <S.ToggleBtn>
-            <S.Top></S.Top>
-            <S.Middle></S.Middle>
-            <S.Bottom></S.Bottom>
-        </S.ToggleBtn>
+        <>
+            <S.ToggleBtn>
+                <S.Top></S.Top>
+                <S.Middle></S.Middle>
+                <S.Bottom></S.Bottom>
+            </S.ToggleBtn>
+            <S.Sidebar/>
+        </>
     )
 }
 
--- front/src/components/Menu/styled.jsx
+++ front/src/components/Menu/styled.jsx
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
+export const Sidebar = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 20rem;
+  background: linear-gradient(to left bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.3));
+  backdrop-filter: blur(2rem);
+  border-radius: 2rem 0 0 2rem;
+`
+
 const thickness = 4;
 
 const Shape = styled(motion.span)`
@@ -9,18 +20,21 @@ const Shape = styled(motion.span)`
   width: 100%;
   height: ${thickness}px;
   border-radius: 3px;
-  background: white;
-`;
+  background: var(--bg-blue);
+`
+
 export const Top = styled(Shape)`
   top: calc(0% + ${thickness / 2}px);
-`;
+`
+
 export const Middle = styled(Shape)`
   top: 50%;
   transform: translateY(-50%);
-`;
+`
+
 export const Bottom = styled(Shape)`
   bottom: calc(0% + ${thickness / 2}px);
-`;
+`
 
 const side = 2.5;
 
@@ -38,4 +52,4 @@ export const ToggleBtn = styled.div`
     opacity: 0.7;
     transform: scale(1.2);
   }
-`;
+`
