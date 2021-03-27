2c2aadc8db77c8ce4e2f844a74a5d86f4229c2f6
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-27 09:47:50 -0300

    Implements appError exceptionHandler

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import 'database'
 
 import express from 'express'
+import 'express-async-errors'
 import cors from 'cors'
 
 import routes from './routes'
+import errorMiddleware from '@middlewares/error'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errorMiddleware)
 
 app.listen(3333, () => {
   console.log('ok')
