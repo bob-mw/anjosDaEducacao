b9f31c54ad4d5487278e4dbea15138d346aa4704
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-27 16:02:50 -0300

    Create guardian entity

---
+++ src/entities/guardian.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity()
+class Guardian {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Guardian
