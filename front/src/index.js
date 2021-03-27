322c09002f0a9f0a2f5a945027a60a447de0ddfa
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-27 09:06:29 -0300

    Separate backend and frontend

--- front/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './css/bootstrap.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
