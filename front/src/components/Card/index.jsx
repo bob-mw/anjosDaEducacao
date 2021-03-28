db4f1619ad0f3e04a1fee71b4938cc121315395c
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-28 17:46:52 -0300

    CardContainer implemented

--- front/src/components/Card/index.jsx
+++ front/src/components/Card/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion, AnimateSharedLayout } from 'framer-motion'
 
 import * as S from './styled'
 
 const Card = () => {
 
     return (
-        
+        <S.CardContainer>
+            <AnimateSharedLayout>
+                {isExpanded ? (
+                <ExpandedCard onCollapse={collapseDate} day={day}>
+                    <Content day={day} disabled={disabled} />
+                </ExpandedCard>
+                ) : (
+                <CompactCard onExpand={expandDate} disabled={disabled} day={day}>
+                    <Content day={day} disabled={disabled} />
+                </CompactCard>
+                )}
+            </AnimateSharedLayout>
+        </S.CardContainer>
     )
 }
 
--- front/src/components/Card/styled.jsx
+++ front/src/components/Card/styled.jsx
@@ -1,1 +1,9 @@
 import styled from 'styled-components'
+
+export const CardContainer = styled.div`
+    width: 7rem;
+    height: 7rem;
+    position: relative;
+    margin-left: 1rem;
+    margin-right: 1rem;
+`
