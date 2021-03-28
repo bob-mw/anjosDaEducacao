c8ccf763277677a853771ef95dfddaddf6f648d3
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-28 16:43:01 -0300

    Update updateGuardian validation

--- src/validation/updateGuardian.ts
+++ src/validation/updateGuardian.ts
@@ -4,7 +4,8 @@ const schema = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().email().required(),
   password: yup.string().required(),
-  newPassword: yup.string().required()
+  newPassword: yup.string().required(),
+  phone: yup.string().required()
 })
 
 export default schema
