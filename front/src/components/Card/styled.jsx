55c877bfa364a48110cc706cc3edf7120694542e
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-28 19:01:02 -0300

    Card Style Component created

--- front/src/components/Card/styled.jsx
+++ front/src/components/Card/styled.jsx
@@ -7,3 +7,17 @@ export const CardContainer = styled.div`
     margin-left: 1rem;
     margin-right: 1rem;
 `
+
+const Card = styled.div`
+  overflow: hidden;
+  box-shadow: 0 0 10px -2px rgba(0, 0, 0, 0.2);
+  padding: 2rem;
+  border-radius: 1rem;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  position: relative;
+  cursor: pointer;
+`
