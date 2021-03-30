315ec832bb89a84803753ada1370b97659436fe4
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-30 08:58:24 -0300

    Added Data File

---
+++ src/Pages/Mural/data.js
@@ -0,0 +1,68 @@
+export const items = [
+  // Photo by ivan Torres on Unsplash
+  {
+    id: "c",
+    name: "Pedro",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 80,
+    backgroundColor: "#814A0E"
+  },
+  // Photo by Dennis Brendel on Unsplash
+  {
+    id: "f",
+    name: "Jonathas",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 120,
+    backgroundColor: "#959684"
+  },
+  // Photo by Alessandra Caretto on Unsplash
+  {
+    id: "a",
+    name: "Paulo",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 260,
+    backgroundColor: "#5DBCD2"
+  },
+  // Photo by Taneli Lahtinen on Unsplash
+  {
+    id: "g",
+    name: "Maria",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 200,
+    backgroundColor: "#8F986D"
+  },
+  // Photo by Simone Hutsch on Unsplash
+  {
+    id: "d",
+    name: "Julia",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 150,
+    backgroundColor: "#FA6779"
+  },
+  // Photo by Siora Photography on Unsplash
+  {
+    id: "h",
+    name: "Ricardo",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 60,
+    backgroundColor: "#282F49"
+  },
+  // Photo by Yerlin Matu on Unsplash
+  {
+    id: "e",
+    name: "Iasmin",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 200,
+    backgroundColor: "#AC7441"
+  },
+  // Photo by Ali Abdul Rahman on Unsplash
+  {
+    id: "b",
+    name: "Tatiana",
+    title: "10 anos, São Paulo",
+    pointOfInterest: 260,
+    backgroundColor: "#CC555B"
+  }
+];
+
+export const openSpring = { type: "spring", stiffness: 200, damping: 30 };
