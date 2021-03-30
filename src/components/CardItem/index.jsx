1f6163b2a4fbcaecfe336ba86f9e0fa5b8cc9d95
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-30 08:57:07 -0300

    Added CardItem Component

---
+++ src/components/CardItem/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { LoremIpsum } from 'react-lorem-ipsum'
+import { Link } from 'react-router-dom'
+
+import * as S from './styled'
+import { items } from '../data'
+
+
+export const CardItem = ({ id }) => {
+  const { name, title } = items.find(item => item.id === id);
+
+  return (
+    <>
+      <S.Overlay
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0, transition: { duration: 0.15 } }}
+        transition={{ duration: 0.2, delay: 0.15 }}
+        style={{ pointerEvents: "auto" }}
+      >
+        <Link to="/" />
+      </S.Overlay>
+      <S.CardContentContainer>
+        <S.CardContent layoutId={`card-container-${id}`}>
+          <S.CardImageContainer
+            layoutId={`card-image-container-${id}`}
+          >
+            <img className="card-image" src={`images/${id}.jpg`} alt="" />
+          </S.CardImageContainer>
+          <S.TitleContainer
+            layoutId={`title-container-${id}`}
+          >
+            <S.Name>{name}</S.Name>
+            <h2>{title}</h2>
+          </S.TitleContainer>
+          <S.ContentContainer animate>
+            <LoremIpsum
+              p={3}
+              avgWordsPerSentence={6}
+              avgSentencesPerParagraph={4}
+            />
+          </S.ContentContainer>
+        </S.CardContent>
+      </S.CardContentContainer>
+    </>
+  );
+}
---
+++ src/components/CardItem/styled.jsx
@@ -0,0 +1,87 @@
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
+export const Overlay = styled(motion.div)`
+    z-index: 1;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.8);
+    will-change: opacity;
+    top: 0;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    max-width: 990px;
+
+    a {
+        display: block;
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        width: 100vw;
+        left: 50%;
+
+        transform: translateX(-50%);
+    }
+
+`
+
+export const CardContentContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    display: block;
+    pointer-events: none;
+
+    top: 0;
+    left: 0;
+    right: 0;
+    position: fixed;
+    z-index: 1;
+    overflow: hidden;
+    padding: 40px 0;
+
+    @media only screen and (max-width: 600px) {
+        padding: 0;
+    }
+`
+
+export const CardContent = styled(motion.div)`
+    position: relative;
+    border-radius: 20px;
+    background: #1c1c1e;
+    overflow: hidden;
+    height: auto;
+    width: 100%;
+    max-width: 700px;
+    margin: 0 auto;
+    pointer-events: none;
+`
+
+export const CardImageContainer = styled(motion.div)`
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    height: 420px;
+    width: 100vw;
+`
+
+export const TitleContainer = styled(motion.div)`
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    max-width: 300px;
+`
+
+export const Name = styled.span`
+    color: #fff;
+    font-size: 2.5rem;
+    text-transform: uppercase;
+`
+
+export const ContentContainer = styled(motion.div)`
+    padding: 460px 35px 35px 35px;
+    max-width: 700px;
+    width: 90vw;
+`
