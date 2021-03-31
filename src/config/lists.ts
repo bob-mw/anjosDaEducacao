ffb14f72fe612940ff4e43746cc403aacb58ffcb
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-03-31 16:31:04 -0300

    Create baseMaterialList in config

---
+++ src/config/lists.ts
@@ -0,0 +1,189 @@
+export default {
+  EI: [
+    {
+      name: 'Pacote camex 500 fls',
+      amount: 1
+    },
+    {
+      name: 'Tubo de cola',
+      amount: 1
+    },
+    {
+      name: 'Rolo fita crepe 50mm x 50mm',
+      amount: 1
+    },
+    {
+      name: 'Caixa de giz de cera grande',
+      amount: 1
+    },
+    {
+      name: 'Tesoura sem ponta',
+      amount: 1
+    },
+    {
+      name: 'Cx de lápis de cor 12 cores',
+      amount: 2
+    },
+    {
+      name: 'Pincel tigre n12',
+      amount: 1
+    },
+    {
+      name: 'Lancheira',
+      amount: 1
+    },
+    {
+      name: 'Agenda',
+      amount: 1
+    }
+  ],
+
+  F1: [
+    {
+      name: 'Caderno universitário 200 folhas pautadas',
+      amount: 4
+    },
+    {
+      name: 'Régua 30cm',
+      amount: 1
+    },
+    {
+      name: 'Estojo',
+      amount: 1
+    },
+    {
+      name: 'Tesoura sem ponta',
+      amount: 1
+    },
+    {
+      name: 'Borracha',
+      amount: 1
+    },
+    {
+      name: 'Apontador',
+      amount: 1
+    },
+    {
+      name: 'Lápis preto',
+      amount: 3
+    },
+    {
+      name: 'Cx de lápis de cor 12 cores',
+      amount: 1
+    },
+    {
+      name: 'Tubo de cola',
+      amount: 1
+    },
+    {
+      name: 'Mochila',
+      amount: 1
+    }
+  ],
+
+  F2: [
+    {
+      name: 'Caderno universitário 200 folhas pautadas',
+      amount: 4
+    },
+    {
+      name: 'Caderno educação artística',
+      amount: 1
+    },
+    {
+      name: 'Régua 30cm',
+      amount: 1
+    },
+    {
+      name: 'Estojo',
+      amount: 1
+    },
+    {
+      name: 'Tesoura sem ponta',
+      amount: 1
+    },
+    {
+      name: 'Borracha',
+      amount: 1
+    },
+    {
+      name: 'Apontador',
+      amount: 1
+    },
+    {
+      name: 'Lápis preto',
+      amount: 3
+    },
+    {
+      name: 'Caneta azul',
+      amount: 2
+    },
+    {
+      name: 'Caneta preta',
+      amount: 2
+    },
+    {
+      name: 'Cx de lápis de cor 12 cores',
+      amount: 1
+    },
+    {
+      name: 'Tubo de cola',
+      amount: 1
+    },
+    {
+      name: 'Mochila',
+      amount: 1
+    }
+  ],
+
+  EM: [
+    {
+      name: 'Caderno universitário 200 folhas pautadas',
+      amount: 4
+    },
+    {
+      name: 'Régua 30cm',
+      amount: 1
+    },
+    {
+      name: 'Estojo',
+      amount: 1
+    },
+    {
+      name: 'Tesoura sem ponta',
+      amount: 1
+    },
+    {
+      name: 'Borracha',
+      amount: 1
+    },
+    {
+      name: 'Apontador',
+      amount: 1
+    },
+    {
+      name: 'Lápis preto',
+      amount: 3
+    },
+    {
+      name: 'Caneta azul',
+      amount: 2
+    },
+    {
+      name: 'Caneta preta',
+      amount: 2
+    },
+    {
+      name: 'Cx de lápis de cor 12 cores',
+      amount: 1
+    },
+    {
+      name: 'Cola bastão',
+      amount: 1
+    },
+    {
+      name: 'Mochila',
+      amount: 1
+    }
+  ]
+}
