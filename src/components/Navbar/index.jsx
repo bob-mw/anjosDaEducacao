6874320545f6ab0c7bba608ac3de39b9f942f7fa
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-31 15:17:13 -0300

    Navbar Container implemented

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Menu from '../Menu'
+import Logo from '../Logo'
+import * as S from './styled'
+
+const Navbar = () => {
+    
+    return (
+        <S.Container>
+            
+        </S.Container>
+    )
+}
+
+export default Navbar
+
---
+++ src/components/Navbar/styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.nav`
+    position: absolute;
+    width: 100%;
+    height: 6rem;
+    top: 0;
+    left: 0;
+`
+
