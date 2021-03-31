78cca18771a668d538bc6409f9625b260f18f616
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-03-31 14:57:58 -0300

    New colors background implemented

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const Layout = () => {
+
+    return (
+        <S.Background>
+            
+        </S.Background>
+    )
+}
+
+export default Layout
---
+++ src/components/Layout/styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Background = styled.main`
+    min-height: 100vh;
+    background: linear-gradient(to right bottom, rgb(241, 215, 216), rgb(89, 69, 70));
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
