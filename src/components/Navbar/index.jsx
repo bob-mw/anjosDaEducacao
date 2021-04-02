25e3c0b7d49fae34a8ddd5f093288b5866505268
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-02 00:07:04 -0300

    useEffect and useRef added in Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { gsap } from 'gsap'
 import { TextPlugin } from 'gsap/TextPlugin'
 
@@ -6,12 +6,19 @@ import Menu from '../Menu'
 import * as S from './styled'
 
 const Navbar = () => {
+
+    let name = useRef(null)
+
+    useEffect(() => {
+
+
+    }, [])
     
     return (
         <S.Container>
             <S.Wrapper>
                 <S.LogoNavbar/>
-                <S.Name>Anjos da Educação</S.Name>
+                <S.Name ref={name}>Anjos da Educação</S.Name>
             </S.Wrapper>
             <Menu/>
         </S.Container>
