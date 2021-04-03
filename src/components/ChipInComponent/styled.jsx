64b45ddc4de441e347819b96655d648aa8b6976b
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-03 15:44:02 -0300

    Implemented Container in ChipIn

---
+++ src/components/ChipInComponent/styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.form`
+    position: relative;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+    padding: 3rem;
+    margin: 5rem 1rem;
+    border-radius: 2rem;
+    background: linear-gradient(to left bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.5));
+    box-shadow:0 0.5rem 1rem rgba(0, 0, 0, 0.3);
+`
