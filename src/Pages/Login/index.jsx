070797106f4d7e13a4712953af9bb96a27a351e2
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-03 00:46:38 -0300

    Hero text animation done

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
+import gsap from 'gsap'
+
 import FormsLogin from '../../components/FormsLogin/index';
 import angel from '../../img/angel.svg';
-import Jumbotron from '../../components/Jumbotron';
-
 import Footer from '../../components/Footer';
 
 import * as S from './styled'
 
 const Login = () => {
+
+    let timeline = useRef()
+    let heroTexts = useRef([])
+
+    const addToHeroTexts = (el) => {
+        if (el && !heroTexts.current.includes(el)) {
+          heroTexts.current.push(el)
+        }
+    }
+    //let timeline = useState(new TimelineMax({paused: true})) //Outra possibilidade
+
+    useEffect(()=>{
+
+        timeline.current = gsap.timeline({ease: "SlowMo.easeOut"})
+        .to(heroTexts.current, {
+            y: "0%",
+            duration: 0.7,
+            stagger: 0.2,
+            delay: 2
+        }, "0")
+        
+    },[])
+
     return (
         <>
             <S.HeroContainer>
-                <S.HeroText><span>Você não pode mudar o mundo,</span></S.HeroText>
-                <S.HeroText><span>mas pode mudar o mundo de alguém!</span></S.HeroText>
+                <S.HeroText><span ref={addToHeroTexts}>Você não pode mudar o mundo,</span></S.HeroText>
+                <S.HeroText><span ref={addToHeroTexts}>mas pode mudar o mundo de alguém!</span></S.HeroText>
             </S.HeroContainer>
             <S.Image src={ angel } alt="logo"/>
             <FormsLogin />
