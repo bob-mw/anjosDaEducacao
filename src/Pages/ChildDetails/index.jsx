15cd605f84524793ce3d5e9860150f7737cb0bc1
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-03 20:17:07 -0300

    ChildDetails pattern file changed

--- src/Pages/ChildDetails/index.jsx
+++ src/Pages/ChildDetails/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import ChipIn from '../components/ChipInComponent/'
-import fakeData from '../data/fakeDbChildrens'
+import ChipIn from '../../components/ChipInComponent/'
+import fakeData from '../../data/fakeDbChildrens'
 
-function ChildDetails() {
+const ChildDetails = () => {
     return (
-        <div className="d-flex flex-column align-items-center">
+        <>
 
-            <section>
-                <h1 className="display-4">Você tem { fakeData.length } filhos cadastrados</h1>
-            </section>
+            <h1 className="display-4">Você tem { fakeData.length } filhos cadastrados</h1>
 
             <label htmlFor="yourChildrens" className="my-5">
 
@@ -23,8 +21,8 @@ function ChildDetails() {
             </label>
 
             <ChipIn/>
-        </div>
-    );
+        </>
+    )
 }
 
-export default ChildDetails;
+export default ChildDetails
