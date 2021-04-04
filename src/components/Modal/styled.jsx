85614148ce6d279b198d072701e022c2920b9e11
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-04 19:23:00 -0300

    Created Menu

--- src/components/Modal/styled.jsx
+++ src/components/Modal/styled.jsx
@@ -2,21 +2,23 @@ import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
 const Overlay = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
 
-export const Panel = styled.div`
+export const Panel = styled(motion.div)`
     position: relative;
     width: 40%;
     height: 40%;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     z-index: 10;
 
     border-radius: 2rem;
@@ -24,33 +26,44 @@ export const Panel = styled.div`
     box-shadow:0 1rem 1rem rgba(128, 91, 91, 0.5); 
 `
 
-const ModalContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  line-height: 1.8;
-  color: #141414;
+const thickness = 5;
 
-  p {
-    margin-bottom: 1rem;
-  }
-
-  button {
-    padding: 10px 24px;
-    background: #141414;
-    color: #fff;
-    border: none;
-  }
+const Shape = styled(motion.span)`
+    display: inline-block;
+    position: absolute;
+    right: 0;
+    width: 100%;
+    height: ${thickness}px;
+    border-radius: 3px;
+    background: var(--bg-darker-orange);
 `
 
-const CloseModalButton = styled(MdClose)`
-  cursor: pointer;
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  width: 32px;
-  height: 32px;
-  padding: 0;
-  z-index: 10;
+export const Top = styled(Shape)`
+    top: 50%;
+    transform: rotate(45deg);
+    width: 130%;
+`
+
+export const Bottom = styled(Shape)`
+    top: 50%;
+    transform: rotate(-45deg);
+    width: 130%;
+`
+
+const side = 2;
+
+export const Menu = styled(motion.div)`
+    position: absolute;
+    cursor: pointer;
+    right: 0.5rem;
+    top: 0.5rem;
+    margin: 1.5rem;
+    width: ${side}rem;
+    height: ${side}rem;
+    transition: transform 0.2s, opacity 0.2s !important;
+    will-change: transform;
+    &:hover {
+        opacity: 0.7;
+        transform: scale(1.2);
+    }
 `
