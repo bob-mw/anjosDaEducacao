d9984781e68100ede5a5fb38198a51cf3b40b9aa
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-04-04 02:47:41 -0300

    Using express.static to mural photos

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import { randomBytes } from 'crypto'
 import path from 'path'
 import multer from 'multer'
 
+const destination = path.resolve(__dirname, '..', '..', 'tmp')
+
 export default {
+  destination,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination,
     filename (request, file, callback) {
       const fileHash = randomBytes(20).toString('hex')
 
--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,13 @@ import cors from 'cors'
 import routes from './routes'
 import errorMiddleware from '@middlewares/error'
 
+import uploadConfig from '@config/upload'
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/file', express.static(uploadConfig.destination))
 app.use(routes)
 app.use(errorMiddleware)
 
