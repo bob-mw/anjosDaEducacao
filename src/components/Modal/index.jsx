0a4a5f0e210e76dba6f05d57f2c937b70a25b707
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-05 10:18:24 -0300

    Created TwoButtonModal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useCallback } from 'react'
 import * as S from './styled'
 
-const Modal = ({ title, subtitle, showModal, setShowModal }) => {
+export const OneButtonModal = ({ title, subtitle, buttonName, showModal, setShowModal }) => {
 
     const modalRef = useRef()
 
@@ -37,19 +37,70 @@ const Modal = ({ title, subtitle, showModal, setShowModal }) => {
                     <S.Description>{title}</S.Description>
                     <S.Description>{subtitle}</S.Description>
                     <S.ButtonWrapper>
-                    <S.Button className="inverted" >Cadastrar</S.Button>
-                        <S.Button
-                            aria-label='Close modal'
-                            onClick={() => setShowModal(prev => !prev)}
-                        >
-                            Não
-                        </S.Button>
+                      <S.Button className="inverted" >{buttonName}</S.Button>
+                      <S.Button
+                          aria-label='Close modal'
+                          onClick={() => setShowModal(prev => !prev)}
+                      >
+                          Não
+                      </S.Button>
                     </S.ButtonWrapper>
                 </S.Panel>
             </S.Overlay>
         ) : null}
       </>
     )
-  }
+}
 
-export default Modal
+export const TwoButtonModal = ({ title, subtitle, firstButtonName, secondButtonName, showModal, setShowModal }) => {
+
+  const modalRef = useRef()
+
+  const closeModal = e => {
+    if (modalRef.current === e.target) {
+      setShowModal(false)
+    }
+  }
+  
+  const keyPress = useCallback(
+      e => {
+      if (e.key === 'Escape' && showModal) {
+          setShowModal(false)
+          console.log('I pressed')
+      }
+      },
+      [setShowModal, showModal]
+  )
+
+  useEffect(
+      () => {
+        document.addEventListener('keydown', keyPress);
+        return () => document.removeEventListener('keydown', keyPress);
+      },
+      [keyPress]
+    )
+
+  return (
+    <>
+      {showModal ? (
+          <S.Overlay  onClick={closeModal} ref={modalRef}>
+              <S.Panel>
+                  <S.Description>{title}</S.Description>
+                  <S.Description>{subtitle}</S.Description>
+                  <S.ButtonWrapper>
+                    <S.Button>{firstButtonName}</S.Button>       
+                    <S.Button>{secondButtonName}</S.Button>   
+                  </S.ButtonWrapper>
+                  <S.Menu
+                      aria-label='Close modal'
+                      onClick={() => setShowModal(prev => !prev)}
+                  >
+                    <S.Top/>
+                    <S.Bottom/>
+                  </S.Menu>
+              </S.Panel>
+          </S.Overlay>
+      ) : null}
+    </>
+  )
+}
