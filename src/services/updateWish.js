11853a5e7c5390cc056f4aae586048bf3a225c4d
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-04-05 10:58:08 -0300

    Add try catch to updateWish service

--- src/services/updateWish.js
+++ src/services/updateWish.js
@@ -1,37 +1,43 @@
 import axios from 'axios';
 
 class UpdateWishService {
-    async execute(
-      {
-        name,
-        schoolName,
-        state,
-        city,
-        teaching,
-        materials,
-        id,
-        token
-      }
-      ) {
-      const data = await axios.put('http://localhost:3333/wish', {
-        data: {
-          name,
-          schoolName,
-          state,
-          city,
-          teaching,
-          materials,
-          id
-        },
-        headers: {
-          'Authorization': `token ${token}`
-        }
-      });
-
-      const { wish } = data.data
-
-      return wish;
+  async execute(
+    {
+      name,
+      schoolName,
+      state,
+      city,
+      teaching,
+      materials,
+      id,
+      token
     }
+    ) {
+      try {
+        const data = await axios.put('http://localhost:3333/wish', {
+          data: {
+            name,
+            schoolName,
+            state,
+            city,
+            teaching,
+            materials,
+            id
+          },
+          headers: {
+            'Authorization': `token ${token}`
+          }
+        });
+        
+        const { wish } = data.data
+        
+        return { error: false, wish };
+      }
+      catch (err) {
+        const { error } = err.response.data
+        return { error, wish: false }
+      }
+  }
 }
 
 export default new UpdateWishService();
