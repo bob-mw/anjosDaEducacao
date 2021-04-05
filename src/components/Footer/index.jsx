15470459bb6931fa676f9bcb90b324d982b394d6
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-04 22:55:18 -0300

    Styled Footer increased

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 import * as S from './styled'
 
+
+
 const Footer = () => {
 
     return(
         <S.FooterContainer>
-
+            <S.PapelariaContainer>
+                <S.Description>Nossos parceiros:</S.Description>
+            </S.PapelariaContainer>
         </S.FooterContainer>
     )
 }
--- src/components/Footer/styled.jsx
+++ src/components/Footer/styled.jsx
@@ -2,11 +2,26 @@ import styled from 'styled-components'
 
 export const FooterContainer = styled.div`
     position: relative;
-    height: 20rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    min-height: 30rem;
     width: 100%;
     margin-top: 5rem;
     border-radius: 0 0 2rem 2rem;
     background: linear-gradient(to top, rgba(243, 131, 117, 1), rgba(243, 131, 117, 0.9), 
     rgba(243, 131, 117, 0.7), rgba(243, 131, 117, 0));
 
+`
+export const PapelariaContainer = styled.div`
+    position: relative;
+    display: grid;
+    grid: 0.2fr 1fr / 1fr 1fr 1fr;
+    width: 100%;
+    grid-gap: 1rem;
+`
+
+export const Description = styled.p`
+    color: white;
+    font-size: 2em;
+    text-transform: uppercase;
 `
