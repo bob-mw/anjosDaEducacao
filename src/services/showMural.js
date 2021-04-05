780aedfd0022e447e6f20e3b24e715252fa69c93
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-04-05 09:32:58 -0300

    Create showMural service

---
+++ src/services/showMural.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+class ShowMuralService {
+  async execute() {
+    const data = await axios.get('http://localhost:3333/mural');
+
+    const { mural } = data.data;
+
+    return mural;
+  }
+}
+
+export default new ShowMuralService();
