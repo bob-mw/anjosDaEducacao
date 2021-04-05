a8550a1ae1ed7ef2eae42176bc2a8bce79cd281a
GuilhermeBensi <guilhermebensi321@gmail.com>
2021-04-05 09:40:07 -0300

    Create showAvailableWishes service

---
+++ src/services/showAvailableWishes.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+class ShowAvailableWishesService {
+  async execute() {
+    const data = await axios.get('http://localhost:3333/donation');
+
+    const { data: donations } = data;
+
+    return donations;
+  }
+}
+
+export default new ShowAvailableWishesService();
