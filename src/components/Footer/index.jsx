28707b27089fc130056b5cac8e4c0784f8171274
Marcus Rodrigues <marcus.v.rodrigues@outlook.com>
2021-04-05 00:27:17 -0300

    Footer responsivity updated

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,20 +8,22 @@ import gmail from '../../img/gmail.svg'
 const Footer = () => {
 
     return(
-        <S.FooterContainer>
-            <S.PapelariaContainer>
-                <S.Description>Nossos parceiros:</S.Description>
-                <S.Image src={'/papelaria/logopapelaria1.jpg'}></S.Image>
-                <S.Image src={'/papelaria/logopapelaria2.jpg'}></S.Image>
-                <S.Image src={'/papelaria/logopapelaria3.jpg'}></S.Image>
-            </S.PapelariaContainer>
-            <S.SocialMediaContainer>
-            <S.Description>Nossas redes:</S.Description>
-            <S.Image src={facebook}></S.Image>
-            <S.Image src={instagram}></S.Image>
-            <S.Image src={gmail}></S.Image>
-            </S.SocialMediaContainer>
-        </S.FooterContainer>
+        <S.Footer>
+            <S.FooterContainer>
+                <S.PapelariaContainer>
+                    <S.Description>Nossos parceiros:</S.Description>
+                    <S.Image src={'/papelaria/logopapelaria1.jpg'}></S.Image>
+                    <S.Image src={'/papelaria/logopapelaria2.jpg'}></S.Image>
+                    <S.Image src={'/papelaria/logopapelaria3.jpg'}></S.Image>
+                </S.PapelariaContainer>
+                <S.SocialMediaContainer>
+                    <S.Description>Nossas redes:</S.Description>
+                    <S.Image src={facebook}></S.Image>
+                    <S.Image src={instagram}></S.Image>
+                    <S.Image src={gmail}></S.Image>
+                </S.SocialMediaContainer>
+            </S.FooterContainer>
+        </S.Footer>
     )
 }
 
--- src/components/Footer/styled.jsx
+++ src/components/Footer/styled.jsx
@@ -1,15 +1,32 @@
 import styled from 'styled-components'
 
-export const FooterContainer = styled.div`
+export const Footer = styled.div`
     position: relative;
-    display: grid;
-    grid-template-columns: 2fr 1.2fr;
-    grid-gap: 2rem;
+    display: flex;
+    justify-content: center;
     min-height: 20rem;
     width: 100%;
     margin-top: 5rem;
     border-radius: 2rem;
     background: linear-gradient(to right bottom, var(--bg-orange), var(--bg-dark-orange));
+
+`
+
+export const FooterContainer = styled.div`
+    position: relative;
+    display: grid;
+    grid-template-columns: 2fr 1.2fr;
+    grid-gap: 2rem;
+    height: 100%;
+    width: 80%;
+
+    @media only screen and (max-width: 1400px) {
+        width: 90%;
+    }
+
+    @media only screen and (max-width: 1000px) {
+        grid-template-columns: 1fr;
+    }
 
 `
 export const PapelariaContainer = styled.div`
@@ -20,7 +37,20 @@ export const PapelariaContainer = styled.div`
     "image image image" 1fr
     / 1fr 1fr 1fr;
     grid-gap: 1rem;
+    width: auto;
     margin: 2rem;
+
+    @media only screen and (max-width: 700px) {
+        grid-template:
+        "title" 0.2fr
+        "image" 1fr
+        "image" 1fr
+        "image" 1fr
+        / 1fr;
+
+        justify-content: start;
+        justify-items: start;
+    }
 `
 
 export const SocialMediaContainer = styled(PapelariaContainer)`
@@ -33,14 +63,23 @@ export const Description = styled.p`
     font-size: 2em;
     text-transform: uppercase;
     align-self: center;
+
+    @media only screen and (max-width: 700px) {
+        align-self: start;
+    }
 `
 
 export const Image = styled.img`
     position: relative;
     display: block;
     height: auto;
+    max-height: 6rem;
     width: 100%;
-    border-radius: 2rem;
     object-fit: contain;
     align-self: start;
+
+    @media only screen and (max-width: 700px) {
+        object-position: 0% 50%;
+    }
+
 `
